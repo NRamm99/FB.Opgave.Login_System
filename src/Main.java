acbc754bcb91eb494e274f5fda7e3710a45736ee
NRamm99 <162293775+NRamm99@users.noreply.github.com>
2025-09-26 19:11:56 +0200

    Ekstra opgaver løst

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,51 @@ import java.util.stream.IntStream;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
-    static String[] usernames = {"Nichlas", "Mark", "Morten", "Mikkel"};
-    static String[] passwords = {"Ramm", "Kordon", "Larsen", "Illemann"};
+    static String[] usernames = {"Nichlas", "Mark", "Morten", "Mikkel", "", "", "", "", "", ""};
+    static String[] passwords = {"Ramm", "Kordon", "Larsen", "Illemann", "", "", "", "", "", ""};
     static int MAX_ATTEMPTS = 3;
+    static boolean entryGranted = false;
 
     public static void main(String[] args) {
 
-        promptLogin();
+        while(!entryGranted){
+            promptMenu();
+        }
+    }
 
+    public static void promptMenu() {
+            printToConsole("""
+                    ---------- Vælg handling ----------
+                    1... Forsøg indgang ved dørmand
+                    2... ADMIN: Rediger gæsteliste
+                    3... Tag hjem igen
+                    """);
+            while (!input.hasNextInt()) {
+                printToConsole("""
+                        ---------- Vælg handling ----------
+                        1... Forsøg indgang ved dørmand
+                        2... ADMIN: Rediger gæsteliste
+                        3... Tag hjem igen
+                        """);
+                printToConsole("FEJL - indtast et gyldigt tal.", false);
+            }
+            int userInput = input.nextInt();
+            switch (userInput) {
+                case 1:
+                    promptLogin();
+                    break;
+                case 2:
+                    promptEdit();
+                    break;
+                case 3:
+                    break;
+            }
     }
 
     public static void promptLogin() {
         printToConsole("Dørmand: Hvad hedder du?");
         System.out.print("Jeg hedder ");
+        input.nextLine();
         String userInput = input.nextLine();
         String user = getUser(userInput);
         if (user == null) {
@@ -24,10 +56,10 @@ public class Main {
             return;
         }
 
-        printToConsole("Dørmand: Tak for det, " + user + ".", false);
+        printToConsole("\nDørmand: Tak for det, " + user + ".", false);
         boolean passwordIsCorrect = false;
         int attempts = 0;
-        while (!passwordIsCorrect && attempts < MAX_ATTEMPTS) {
+        while ((!passwordIsCorrect) && (attempts < MAX_ATTEMPTS)) {
             printToConsole("\nDørmand: Hvad er dit kodeord?", false);
             System.out.print("Forsøg " + (attempts + 1) + ": ");
             userInput = input.nextLine();
@@ -37,10 +69,12 @@ public class Main {
 
         if (!passwordIsCorrect) {
             printToConsole("Dørmand: Det var desværre dine 3 forsøg.\nDørmand: Forlad venligst området");
+            waitForUser();
             return;
         }
 
         printToConsole("Dørmand: Det er korrekt.\nDørmand: Velkommen til, " + user + ". \nDørmand: Vi har glædet os til at se dig.");
+        entryGranted = true;
     }
 
     private static boolean checkPassWord(String userInput, String user) {
@@ -75,4 +109,108 @@ public class Main {
             System.out.println();
         }
     }
+
+    public static void promptEdit() {
+        printToConsole("""
+                ---------- Vælg handling ----------
+                    1... Tilføj bruger
+                    2... Fjern bruger
+                    3... Rediger bruger
+                -----------------------------------
+                   \s""");
+        int userInput = input.nextInt();
+        switch (userInput) {
+            case 1:
+                addUser();
+                break;
+            case 2:
+                removeUser();
+                break;
+            case 3:
+                editUser();
+                break;
+        }
+    }
+
+    public static void addUser() {
+        int takenCounter = 0;
+        String username;
+        String password;
+        for (int n = 0; n <= usernames.length; n++) {
+            if (usernames[n].equalsIgnoreCase("")) {
+                printToConsole("Indtast navn på gæsten du vil tilføje:");
+                System.out.print("Jeg ønsker at tilføje: ");
+                input.nextLine();
+                username = input.nextLine();
+
+                printToConsole("Angiv venligst adgangskode til " + username + ".");
+                System.out.print(username + " adgangskode: ");
+                password = input.nextLine();
+
+                usernames[n] = username;
+                passwords[n] = password;
+
+                printToConsole("Du har nu tilføjet:\nBrugernavn: " + username + "\nAdgangskode: " + password);
+                waitForUser();
+                return;
+            } else {
+                takenCounter++;
+            }
+        }
+        if (takenCounter == 10) {
+            printToConsole("""
+                    Der er desværre ikke flere ledige pladser på gæstelisten.
+                    Fjern eller rediger en fra listen.
+                    """);
+        }
+    }
+
+    public static void removeUser() {
+        printUserList();
+        while (true) {
+            printToConsole("\nIndtast venligst navnet på den bruger du ønsker at fjerne.");
+            System.out.print("Jeg ønsker af fjerne: ");
+            String userInput = input.nextLine();
+            for (int n = 0; n <= usernames.length - 1; n++) {
+                if (userInput.equalsIgnoreCase(usernames[n])) {
+                    usernames[n] = "";
+                    passwords[n] = "";
+                    return;
+                }
+            }
+            System.out.println("Navn ikke fundet. Prøv igen.");
+        }
+    }
+
+    private static void editUser() {
+        while (true) {
+            printToConsole("\nIndtast venligst navnet på den bruger du ønsker at redigere.");
+            System.out.print("Jeg ønsker af redigere: ");
+            String userInput = input.nextLine();
+            for (int n = 0; n <= usernames.length - 1; n++) {
+                if (userInput.equalsIgnoreCase(usernames[n])) {
+                    printToConsole("Du redigerer lige nu:\n" + usernames[n] + "\n" + passwords[n]);
+                    System.out.print("\nIndtast nye navn: ");
+                    usernames[n] = input.nextLine();
+                    System.out.print("\nIndtast nye adgangskode: ");
+                    passwords[n] = input.nextLine();
+                    return;
+                }
+            }
+            System.out.println("Navn ikke fundet. Prøv igen.");
+        }
+    }
+
+    public static void waitForUser() {
+        System.out.println("\nTryk enter for at fortsætte...");
+        input.nextLine();
+    }
+
+    public static void printUserList() {
+        for (int n = 0; n <= usernames.length; n++) {
+            printToConsole("---------- GÆSTELISTE ----------");
+            System.out.println(n + 1 + ": " + usernames[n]);
+        }
+    }
+
 }
